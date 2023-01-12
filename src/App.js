3e854769e7ff2d419a130d70e578c5765dbe1cf4
Akua Appiah-Lindberg <appiah-lindberg@outlook.com>
2023-01-12 16:01:32 +0100

    create todos with useReducer and add toggleand delete buttons button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, {useReducer, useState} from "react"
 import './App.css';
+import Todo from "./Todo";
 
-function App() {
+const ACTIONS={
+  ADD_TODO:'add-todo',
+  TOGGLE_TODO:'toggle-todo'
+
+}
+function reducer(todos, action){
+switch(action.type){
+  case(ACTIONS.ADD_TODO):
+  return[...todos,newTodo(action.payload.name) 
+  ];
+  case(ACTIONS.TOGGLE_TODO):
+  return todos.map(todo=>{
+    if(todo.id===action.payload.id){
+      return {...todo, complete:!todo.complete}
+    }
+    return todo
+  })
+
+}
+}
+
+function newTodo(name){
+  return{id:Date.now(), name:name, complete:false}
+}
+
+
+export default function App() {
+
+  const [todos, dispatch] = useReducer(reducer, [])
+  const [name, setName] = useState('');
+
+
+
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+dispatch({type:ACTIONS.ADD_TODO, payload:{name:name}})
+setName('')
+
+
+  }
+  console.log(todos)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+      <input type="text" value={name} onChange={e=>setName(e.target.value)}/>       
+      </form>
+     { todos.map(todo=>{
+      return <Todo key={todo.id} todo={todo}/>
+     })}
     </div>
   );
 }
 
-export default App;
+
